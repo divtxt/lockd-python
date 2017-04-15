26f08cdfec3e295961be840f2650eb4c2365a744
Div Shekhar <div@txtlabs.com>
2017-04-15 16:41:37 -0700

    Add client and integration test

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
 # lockd-python
 
-Python client for the [lockd](https://github.com/divtxt/lockd-python) distributed lock service.
+Python client for the [lockd](https://github.com/divtxt/lockd) distributed lock service.
 
+## Example Usage
+
+This assumes a `lockd` server is running on the localhost at the default port.
+
+```
+~/wpy/lockd-python$ python
+Python 2.7.10 (default, Oct 23 2015, 19:19:21)
+[GCC 4.2.1 Compatible Apple LLVM 7.0.0 (clang-700.0.59.5)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+>>> from lockd import LockdClient
+>>> lockd_client = LockdClient()
+>>> lockd_client.is_locked("foo")
+False
+>>> lockd_client.lock("foo")
+True
+>>> lockd_client.lock("foo") # second lock attempt will fail
+False
+>>> lockd_client.is_locked("foo")
+True
+>>> lockd_client.unlock("foo")
+True
+>>>
+```
---
+++ lockd.py
@@ -0,0 +1,40 @@
+# Client for the lockd distributed lock service.
+#
+# See https://github.com/divtxt/lockd-python for details.
+#
+
+import httplib
+import json
+
+class LockdClient(object):
+
+    def __init__(self, host="127.0.0.1", port=2080):
+        self._host_port = "%s:%s" % (host, port)
+
+    def is_locked(self, name):
+        return self._lockish("GET", name, 404)
+
+    def lock(self, name):
+        return self._lockish("POST", name, 409)
+
+    def unlock(self, name):
+        return self._lockish("DELETE", name, 404)
+
+    def _lockish(self, method, name, false_code):
+        # FIXME: does name need to escaped here?
+        path = "/lock/%s" % name
+        #
+        conn = httplib.HTTPConnection(self._host_port)
+        conn.request(method, path)
+        response = conn.getresponse()
+        status = response.status
+        response.read()
+        conn.close()
+        #
+        if status == 200:
+            return True
+        elif status == false_code:
+            return False
+        else:
+            msg = "Unexpected response: %s %s" % (status, response.reason)
+            raise Exception(msg)
